b1dee8bd67c9bced330ccc18c0c2eb4528296bdc
jocelyn alsdorf & Frank Johnson <student@epicodus.com>
2015-04-30 10:21:13 -0700

    writing init and attr_reader methods to allow spec to pass.

--- lib/phone.rb
+++ lib/phone.rb
@@ -1,2 +1,13 @@
 class Phone
+
+  @@phone_info = []
+
+  attr_reader(:area_code, :number, :type)
+
+  define_method(:initialize) do |area_code, number, type|
+    @area_code = area_code
+    @number = number
+    @type = type
+    @id = @@phone_info.length + 1
+  end
 end
--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -6,9 +6,9 @@ describe(Phone) do
   describe('#area_code') do
     it("returns the area_code of a phone number") do
       test_number = Phone.new("503", "7587819", "cell")
-      expect(test_contact.area_code).to(eq("503"))
-      expect(test_contact.number).to(eq("7587819"))
-      expect(test_contact.type).to(eq("cell"))
+      expect(test_number.area_code).to(eq("503"))
+      expect(test_number.number).to(eq("7587819"))
+      expect(test_number.type).to(eq("cell"))
     end
   end
 end
