315a73d9ed912edd128d002ac6cf544a98cd4a0d
jocelyn alsdorf & Frank Johnson <student@epicodus.com>
2015-04-30 14:12:36 -0700

    writing spec to test path from index to contact list page which passes

--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -4,11 +4,11 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('path for contact-index page', {:type => :feature}) do
-  # it('links user to dealership-list page') do
-  #   visit('/')
-  #   click_link('See Dealership List')
-  #   expect(page).to have_content('Dealerships')
-  # end
+  it('links user to contact-list page') do
+    visit('/')
+    click_link('See Contact List')
+    expect(page).to have_content('Contacts')
+  end
 
   it('links user to add-new-contact page') do
     visit('/')
