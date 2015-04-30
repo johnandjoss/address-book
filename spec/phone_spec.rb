6179dd7c69f5aa9bc6c27117be1f9a38bfd4a88c
jocelyn alsdorf & Frank Johnson <student@epicodus.com>
2015-04-30 10:28:39 -0700

    wrinting spec for all method which fails as expected.

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -3,12 +3,18 @@ require('phone')
 
 describe(Phone) do
 
-  describe('#area_code') do
-    it("returns the area_code of a phone number") do
+  describe('#initialize', '#attr_reader') do
+    it("returns the details of a phone number") do
       test_number = Phone.new("503", "7587819", "cell")
       expect(test_number.area_code).to(eq("503"))
       expect(test_number.number).to(eq("7587819"))
       expect(test_number.type).to(eq("cell"))
+    end
+  end
+  
+  describe('.all') do
+    it("is empty at first") do
+      expect(Phone.all()).to(eq([]))
     end
   end
 end
