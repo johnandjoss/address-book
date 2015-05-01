c04e467043f8577660caaf330cb03a20c435b237
Guest User <student@epicodus.com>
2015-05-01 15:39:44 -0700

    Made forms for addresses and added routes to route handler

--- app.rb
+++ app.rb
@@ -25,10 +25,43 @@ post('/contacts') do
   birth_month = params.fetch('birth_month')
   Contact.new(last_name, first_name, birth_month).save()
   @contacts = Contact.all
-  erb(:contacts)
+  erb(:success)
 end
 
 get('/contacts/:id') do
   @contact = Contact.find(params.fetch('id').to_i)
   erb(:contact)
+end
+
+get('/contacts/:id/addresses/new') do
+    @contact = Contact.find(params.fetch('id').to_i())
+    erb(:contact_address_form)
+end
+
+get('/contacts/:id/phones/new') do
+    @contact = Contact.find(params.fetch('id').to_i())
+    erb(:contact_phones_form)
+end
+
+post('/addresses') do
+  city = params.fetch('city')
+  state = params.fetch('state')
+  zip = params.fetch('zip')
+  type = params.fetch('type')
+  @address = Address.new(city, state, zip, type)
+  @address.save()
+  @contact = Contact.find(params.fetch('contact_id').to_i())
+  @contact.add_address(@address)
+  erb(:success)
+end
+
+post('/phones') do
+  area_code = params.fetch('area_code')
+  number = params.fetch('number')
+  type = params.fetch('type')
+  @phone = Phone.new(area_code, number, type)
+  @phone.save()
+  @contact = Contact.find(params.fetch('contact_id').to_i())
+  @contact.add_phone(@phone)
+  erb(:success)
 end
